326f0ae637d7074697eec514023e6d8a451a16c5
Vlad Bokii <bokiyvladik10@gmail.com>
2025-11-12 22:21:25 +0200

    create

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export async function pixApi(inputValue, currentPage) {
+    const axiosOptions = {
+        params: {
+            per_page: 15,
+            page: currentPage,
+            key: "48300565-6dad9db0463f535246b89c980",
+            q: inputValue,
+            image_type: "photo",
+            orientation: "horizontal",
+            safesearch: "true",
+        },
+    };
+
+    return axios.get(`https://pixabay.com/api/`, axiosOptions)
+};
